4cc5c60e525c50b0d44915de92c9ebf9737c1298
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2025-05-20 10:37:20 +0200

    continuation

--- alex_main.py
+++ alex_main.py
@@ -28,7 +28,7 @@ def clean_dataframe(df): # rensar df, använder kön och ålder == total, och ta
         df[year] = pd.to_numeric(df[year].str.replace('..', '0'), errors='coerce')
     return df.set_index("utbildningens inriktning")
 
-def create_linechart(df, **options):
+def create_linechart(df, **options): # Skapar en linechart
     df_long = df.reset_index().melt(
         id_vars="utbildningens inriktning",
         var_name="År",
@@ -49,22 +49,42 @@ def create_linechart(df, **options):
     )
     return fig
 
+def update_end_year(state):
+    state.end_year = max(state.start_year, state.end_year)
+
 try:
     df = read_csv(sub_category="2024_kurser", file_name="studerande_examinerad_yrkeshogskola.csv")
 except FileNotFoundError as e:
     print(f"Fel: {e}")
     df = pd.DataFrame() 
  
+# start variabler:
 df_cleaned = clean_dataframe(df)
+years= df_cleaned.columns[3:]
+start_year = int(years[0])
+end_year = int(years[-1]) 
+educational_area = []
 chart = create_linechart(df_cleaned, xlabel= "År", ylabel= "Antal Studerande")
 
-with tgb.Page() as page: 
+with tgb.Page() as number_students_educationalarea_year: #Taipy
     with tgb.part(class_name="container card"):
-        tgb.text("# Studenter på Yrkeshögskola", mode="md")
-        tgb.text("### Utveckling över tid per utvecklingsområde", mode="md")
-        with tgb.part(class_name="card"):
-            tgb.text("Rådata:", mode="md")
-            tgb.table("{df_cleaned}")
-            tgb.chart(figure="{chart}")
+        with tgb.layout(columns="2 1"):
+            with tgb.part(class_name="card"):
+                tgb.text("# Studenter på Yrkeshögskola", mode="md")
+                tgb.text("### Utveckling över tid per utvecklingsområde", mode="md")
+                tgb.chart(figure="{chart}")
+            with tgb.part(class_name="card"):
+                tgb.text("**Filtrera data: År och Utbildnings Område**", mode="md")
+                tgb.text("Välj utbildningsområde:")
+                tgb.selector(value="{educational_area}", lov=df_cleaned.index, dropdown=True, multiple=True)
+                tgb.text("Välj start år:")
+                tgb.slider(value="{start_year}", min=start_year, max=end_year-1, step=1, show_value=True, label="Startår", on_change=update_end_year)
+                tgb.text("Välj slut år:")
+                tgb.slider(value="{end_year}", min="{start_year}", max=end_year, step=1, show_value=True, label="Slutår")
+                
+
+
+
+
 if __name__ == "__main__":
-    Gui(page).run(dark_mode=False, use_reloader=True, port=8080)
+    Gui(number_students_educationalarea_year).run(dark_mode=False, use_reloader=True, port=8080)
