7b61d1f00a2f70034b51db90b016d4681f532ce5
Marcus <marcus.ericsson_92@icloud.com>
2025-05-28 13:10:41 +0200

    start of dashboard all together

--- frontend/page/home.py
+++ frontend/page/home.py
@@ -1,7 +1,16 @@
 import taipy.gui.builder as tgb
+from taipy.gui import Gui
+
 
 with tgb.Page() as home_page:
     tgb.navbar()
-    
-    tgb.text("# Hem", mode = "md")
-
+    tgb.toggle(theme=True)
+    with tgb.part(class_name= "card text-center card-margin"):
+        tgb.text("# Välkommen till **The skool**", mode = "md")
+        
+        
+        
+        
+        
+# if __name__=="__main__":
+#     Gui(home_page, css_file="style.css").run(dark_mode=False, use_reloader=False, port=8080)
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from taipy.gui import Gui
 from frontend.page.karta_dashboard import map_page
 from frontend.page.home import home_page
-# from frontend.page.kurser_dashboard import page
+from frontend.page.anordnare import Organizer
+from frontend.page.alex_main import number_students_educationalarea_year
+from frontend.page.statliga_bidrag import government_grant_per_program
 
-pages = {"Hem": home_page,"Karta": map_page}
 
-Gui(pages=pages).run(port=8080, dark_mode=False, use_reloader=True)
+
+
+pages = {"Hem": home_page, "Anordnare": Organizer, "Karta": map_page, "Studenter per utbildningsområde": number_students_educationalarea_year, "Statsbidrag": government_grant_per_program}
+
+if __name__=="__main__":
+    Gui(pages=pages).run(port=8080, dark_mode=False, use_reloader=True)
